452d4d69c93d5878b261a1e5f59cb27a0068f6a9
luanagreys7 <luanagreys7@gmail.com>
2025-08-13 10:05:58 -0300

    telaacesso sem nav

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import maisOpcoes from './src/screens/maisOpcoes';
 import telaAcesso from './src/screens/telaAcesso';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Opções" component={maisOpcoes} />
+        <Stack.Screen name="Acesso" component={telaAcesso} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/telaAcesso.js
+++ src/screens/telaAcesso.js
@@ -1,10 +1,49 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function telaAcesso() {
     return(
-        <View>
-            
+        <View style={{backgroundColor: #f6f6f6}}>
+            <View>
+                <Image
+                    source={{uri: 'assets\casual_dog.png'}}
+                    style={{width: 100, height: 100}}
+                
+                />
+            </View>
+            {/*Título e subtítulo*/}
+            <View>
+                <Text style={{fontSize: 20}}>
+                    Ótimo dia!
+                </Text>
+                <Text style={{ fontSize: 20}}>
+                    Como deseja acessar?
+                </Text>
+            </View>
+            {/*Botões*/}
+                <TouchableOpacity
+                    style={{backgroundColor: '#14c871', padding: 10}}
+                >
+                    <View>
+                        <View>
+                            <Image
+                                source={{ uri: 'assets\Google.png'}}
+                                style={{width: 100, height: 100 }}
+                            />
+                        </View>
+                        <View>
+                            <Text style={{color: 'white'}}>Como deseja acessar?</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{backgroundColor: '#ffffff', padding: 10}}
+                    onPress={() => }
+                >
+                    <View>
+                        <Text style={{color: 'black'}}>Outras opções</Text>
+                    </View>
+                </TouchableOpacity>
         </View>
     )
 };
