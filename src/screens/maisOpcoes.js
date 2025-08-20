7edbcd6f4c58be16184af826a4f125963582df52
luanagreys7 <luanagreys7@gmail.com>
2025-08-20 08:54:58 -0300

    .

--- src/screens/maisOpcoes.js
+++ src/screens/maisOpcoes.js
@@ -3,13 +3,42 @@ import { Text, View } from 'react-native';
 
 export default function maisOpcoes() {
     return (
-      <View>
         <View>
+            {/*Título e subtítulo*/}
+            <View>
+                <Text style={{fontSize: 20}}>
+                    Acesse
+                </Text>
+                <Text style={{ fontSize: 20}}>
+                    com E-mail e senha
+                </Text>
+            </View>
+            {/*Campos de input*/}
 
-        </View>
-        <View>
-            
-        </View>
-      </View>  
+            {/*Botões*/}
+            <View>
+                <TouchableOpacity style={{backgroundColor: '#14c871', padding: 10}}>
+                        <View>
+                            <View>
+                                <Image
+                                    source={{ uri: 'assets\Google.png'}}
+                                    style={{width: 100, height: 100 }}
+                                />
+                            </View>
+                            <View>
+                                <Text style={{color: 'white'}}>Como deseja acessar?</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{backgroundColor: '#ffffff', padding: 10}}
+                        onPress={() => }
+                    >
+                        <View>
+                            <Text style={{color: 'black'}}>Outras opções</Text>
+                        </View>
+                    </TouchableOpacity>
+            </View>
+        </View>  
     )
 }
